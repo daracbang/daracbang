0aee2d8fa66580a1c28f7355a0c70fcb0cf6d1e2
dener8 <8dener@gmail.com>
2023-10-26 13:39:16 +0900

    feat<server>: 회원 엔터티 구현

--- backend/apiserver/src/main/java/a503/daracbang/domain/member/entity/Member.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/member/entity/Member.java
@@ -1,12 +1,13 @@
 package a503.daracbang.domain.member.entity;
 
-
 import a503.daracbang.global.entity.BaseTimeEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,4 +20,27 @@ public class Member extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String loginId;
+
+    private String password;
+
+    private String nickname;
+
+    @Lob
+    private byte[] profileImage;
+
+    private String introduce;
+
+    @Builder
+    public Member(String loginId, String password, String nickname, byte[] profileImage) {
+        this.loginId = loginId;
+        this.password = password;
+        this.nickname = nickname;
+        this.profileImage = profileImage;
+        this.introduce = null;
+    }
+
+    public void updateIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
 }
