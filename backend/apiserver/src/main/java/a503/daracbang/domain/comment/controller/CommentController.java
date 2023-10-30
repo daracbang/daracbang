2e657fd11102b42e7906e90e73a7d23fd2155271
Haejune Lee <gowns0205@hanyang.ac.kr>
2023-10-30 16:12:39 +0900

    feature<server>: 다이어리, 댓글 엔티티 생성

--- backend/apiserver/src/main/java/a503/daracbang/domain/comment/controller/CommentController.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/comment/controller/CommentController.java
@@ -1,4 +1,12 @@
 package a503.daracbang.domain.comment.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CommentController {
+    // 댓글 작성
+
+    // 댓글 삭제
+
+    // 댓글 조회
 }
