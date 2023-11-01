cd2829cffc872439a1e7679b4a508317e8363a58
Haejune Lee <gowns0205@hanyang.ac.kr>
2023-11-01 16:36:58 +0900

    feature<server>: 다이어리 작성, 삭제 기능(감정분석X)

--- backend/apiserver/src/main/java/a503/daracbang/domain/diary/dto/request/WriteDiaryRequest.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/diary/dto/request/WriteDiaryRequest.java
@@ -5,10 +5,15 @@ import a503.daracbang.domain.diary.entity.Diary;
 import a503.daracbang.domain.diary.entity.Scope;
 import a503.daracbang.domain.member.entity.Member;
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 public class WriteDiaryRequest {
     private String content;
     private Scope scope;
+
+    public WriteDiaryRequest() {
+    }
 
     @Builder
     public WriteDiaryRequest(String content, Scope scope){
@@ -16,10 +21,12 @@ public class WriteDiaryRequest {
         this.scope = scope;
     }
 
-    public Diary toEntity(){
+    public Diary toEntity(Long memberId){
+
         return Diary.builder()
                 .scope(this.scope)
                 .content(this.content)
+                .memberId(memberId)
                 .build();
     }
 }
--- backend/apiserver/src/main/java/a503/daracbang/domain/diary/service/WriteDiaryService.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/diary/service/WriteDiaryService.java
@@ -2,9 +2,16 @@ package a503.daracbang.domain.diary.service;
 
 import a503.daracbang.domain.diary.dto.request.WriteDiaryRequest;
 import a503.daracbang.domain.diary.entity.Diary;
+import a503.daracbang.domain.diary.exception.DiaryAlreadyWrittenException;
+import a503.daracbang.domain.diary.exception.DiaryErrorCode;
 import a503.daracbang.domain.diary.repository.DiaryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,9 +19,18 @@ public class WriteDiaryService {
 
     private final DiaryRepository diaryRepository;
 
-    public void writeDiary(WriteDiaryRequest writeDiaryRequest) {
-        Diary diary = writeDiaryRequest.toEntity();
+    public void writeDiary(Long memberId, WriteDiaryRequest writeDiaryRequest) {
         // todo : 감정 분석
-        diaryRepository.save(diary);
+        LocalDateTime startDatetime = LocalDateTime.of(LocalDate.now().minusDays(1), LocalTime.of(0,0,0));
+        LocalDateTime endDatetime = LocalDateTime.of(LocalDate.now(), LocalTime.of(23,59,59));
+        List<Diary> alreadyWrittenDiary = diaryRepository.findAllByMemberIdAndCreatedAtBetween(memberId,startDatetime,endDatetime);
+        // 이미 오늘 다이어리를 작성한 사용자인 경우
+        if(!alreadyWrittenDiary.isEmpty())
+            throw new DiaryAlreadyWrittenException(DiaryErrorCode.ALREADYWRITTEN_DIARY);
+        // 작성하지 않은 경우에만 저장
+        else{
+            Diary diary = writeDiaryRequest.toEntity(memberId);
+            diaryRepository.save(diary);
+        }
     }
 }
