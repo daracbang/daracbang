f0df4b52b51626c4754e4b1672527cfadd9110a0
우찬희 <8dener@gmail.com>
2023-11-01 13:53:33 +0900

    feature<server>: 회원 가입 로직 구현
    Merge: Merge branch 'feature/BE/member' into 'develop/BE'
    
    * feat<server>: 회원가입 관련 로직 구현
    
    * feat<server>: 프로필이미지 s3용으로 수정
    
    * feat<server>: JWT 관련 로직 구현
    
    * feat<server>: 회원 엔터티 구현
    
    
    See merge request s09-final/S09P31A503!6

--- backend/apiserver/src/main/java/a503/daracbang/domain/member/exception/MemberErrorCode.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/member/exception/MemberErrorCode.java
@@ -3,7 +3,11 @@ package a503.daracbang.domain.member.exception;
 import a503.daracbang.global.exception.ErrorCode;
 
 public enum MemberErrorCode implements ErrorCode {
-    NOTFOUND_MEMBER("MEMBER_001","해당 회원을 찾을 수 없습니다.",404);
+    NOTFOUND_MEMBER("MEMBER_001","해당 회원을 찾을 수 없습니다.",404),
+    DUPLICATE_MEMBER_LOGIN_ID("MEMBER_002", "중복된 아이디입니다.", 409),
+    DUPLICATE_MEMBER_NICKNAME("MEMBER_003", "중복된 닉네임입니다.", 409),
+
+    ;
 
     private String errorCode;
     private String message;
