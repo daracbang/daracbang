3353a46f45cc9ef4fb95e7b3017095f4037c8d16
Haejune Lee <gowns0205@hanyang.ac.kr>
2023-11-06 16:06:09 +0900

    feature<server>: 다이어리, 무드트래커 조회 기능 구현

--- backend/apiserver/src/main/java/a503/daracbang/domain/diary/entity/Scope.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/diary/entity/Scope.java
@@ -2,8 +2,4 @@ package a503.daracbang.domain.diary.entity;
 
 public enum Scope {
     PUBLIC, NEIGHBOR, PRIVATE;
-
-    public boolean isExist(String userInputScope) {
-        return false;
-    }
 }
