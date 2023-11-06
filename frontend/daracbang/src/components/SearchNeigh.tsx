544ee016cf2905111b49cb2a96c170ae3b1229c6
서수정 <ssj946@naver.com>
2023-11-06 17:37:33 +0900

    feat: UI완성

--- frontend/daracbang/src/components/SearchNeigh.tsx
+++ frontend/daracbang/src/components/SearchNeigh.tsx
@@ -8,12 +8,12 @@ import styled from "@emotion/styled";
 export default function SearchNeigh() {
 
     return (
-        <Card style={{ height: "70px", width: "360px", marginLeft: "20px", marginTop: "15px" }}>
+        <Card style={{ height: "70px", width: "360px", margin: "15px auto", boxShadow: "2px 2px 1px 1px #eeeeee", borderRadius: "10px" }}>
             <NeighInfo style={{ display: "flex", flexDirection: "row", marginTop: "10px", marginLeft: "10px" }}>
                 <img src={Sun} alt='sun' style={{ height: "20px", marginLeft: "10px" }} />
                 <Typography style={{ fontFamily: "omyu_pretty", fontWeight: "bold", width: "100px", marginLeft: "10px", marginRight: "100px" }}>김싸피</Typography>
-                <Button><img src={Plus} alt='plus' style={{ height: "20px", marginLeft: "50px" }} /></Button>
-                <Button><img src={Ban} alt='ban' style={{ height: "20px" }} /></Button>
+                <Button style={{ marginLeft: "30px", padding: 0 }}><img src={Plus} alt='plus' style={{ height: "20px" }} /></Button>
+                <Button style={{ padding: 0 }}><img src={Ban} alt='ban' style={{ height: "20px" }} /></Button>
             </NeighInfo>
             <Typography style={{ fontFamily: 'omyu_pretty', marginLeft: "20px" }}>놀러오세요 마이 다락방!</Typography>
         </Card>
