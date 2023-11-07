7d1e14f6aeb0191c0a6df730109feeb8e0f24130
Haejune Lee <gowns0205@hanyang.ac.kr>
2023-11-07 17:55:43 +0900

    feature<server>: 다이어리 조회 권한 설정

--- backend/apiserver/src/main/java/a503/daracbang/domain/diary/service/FindDiaryService.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/diary/service/FindDiaryService.java
@@ -4,9 +4,13 @@ import a503.daracbang.domain.diary.dto.response.DiaryListResponse;
 import a503.daracbang.domain.diary.dto.response.DiaryResponse;
 import a503.daracbang.domain.diary.dto.response.MoodTrackerListResponse;
 import a503.daracbang.domain.diary.entity.Diary;
+import a503.daracbang.domain.diary.entity.Scope;
 import a503.daracbang.domain.diary.exception.DiaryErrorCode;
+import a503.daracbang.domain.diary.exception.DiaryNoPermissionException;
 import a503.daracbang.domain.diary.exception.DiaryNotFoundException;
+import a503.daracbang.domain.diary.exception.DiaryNotWriterException;
 import a503.daracbang.domain.diary.repository.DiaryRepository;
+import a503.daracbang.domain.neighbor.service.NeighborService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,11 +23,19 @@ import java.util.List;
 public class FindDiaryService {
 
     private final DiaryRepository diaryRepository;
+    private final NeighborService neighborService;
 
     public DiaryResponse getDiary(Long requesterId, Long diaryId) {
         Diary diary = diaryRepository.findById(diaryId)
                 .orElseThrow(()->new DiaryNotFoundException(DiaryErrorCode.NOTFOUND_DIARY));
         // todo: 다이어리 공개범위와 사용자 이웃여부 비교해서, 이웃공개나 비공개 처리하기
+        Scope scope = diary.getScope();
+        if(requesterId==diary.getMemberId())
+            return new DiaryResponse(diary);
+        if(scope.equals("PRIVATE"))
+            throw new DiaryNoPermissionException(DiaryErrorCode.NOPERMISSION_DIARY);
+        if(scope.equals("NEIGHBOR") && !neighborService.isNeighbor(requesterId, diary.getMemberId()))
+            throw new DiaryNoPermissionException(DiaryErrorCode.NOPERMISSION_DIARY);
         return new DiaryResponse(diary);
     }
 
