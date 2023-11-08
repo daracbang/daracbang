a3876a4fab112a31da9f7b66692929f8c74bdb6f
권민재 <minjea917@gmail.com>
2023-11-08 16:51:51 +0900

    fix: guetsbook과 member 연관관계 설정
    Merge: Merge branch 'fix/BE/mapping' into 'develop/BE'
    
    * fix: guetsbook과 member 연관관계 설정
    
    
    See merge request s09-final/S09P31A503!26

--- backend/apiserver/src/main/java/a503/daracbang/domain/guestbook/service/CreateGuestbookService.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/guestbook/service/CreateGuestbookService.java
@@ -2,9 +2,13 @@ package a503.daracbang.domain.guestbook.service;
 
 import a503.daracbang.domain.guestbook.dto.request.RegisterGuestbookRequest;
 import a503.daracbang.domain.guestbook.repository.GuestbookRepository;
+import a503.daracbang.domain.member.entity.Member;
+import a503.daracbang.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Transactional(readOnly = true)
 @Service
@@ -13,9 +17,11 @@ public class CreateGuestbookService {
 
     private final GuestbookRepository guestbookRepository;
 
+    private final MemberRepository memberRepository;
+
     @Transactional
     public void save(Long memberId, RegisterGuestbookRequest form) {
-//         현재 Member 정보가 다 구현되지 않아 1L 로 처리
-         guestbookRepository.save(form.toEntity(1L));
+        Optional<Member> member = memberRepository.findById(memberId);
+        guestbookRepository.save(form.toEntity(member.get()));
     }
 }
