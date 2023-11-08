f63ac28569ae9654135a182fb6b253bba17967fe
Haejune Lee <gowns0205@hanyang.ac.kr>
2023-11-08 14:29:53 +0900

    rename<server>: CommentRepository 오타 수정

--- backend/apiserver/src/main/java/a503/daracbang/domain/comment/service/FindCommentService.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/comment/service/FindCommentService.java
@@ -2,7 +2,7 @@ package a503.daracbang.domain.comment.service;
 
 import a503.daracbang.domain.comment.dto.response.CommentListResponse;
 import a503.daracbang.domain.comment.entity.Comment;
-import a503.daracbang.domain.comment.repository.CommentRepositoty;
+import a503.daracbang.domain.comment.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,9 +14,9 @@ import java.util.List;
 @Transactional(readOnly = true)
 public class FindCommentService {
 
-    private final CommentRepositoty commentRepositoty;
+    private final CommentRepository commentRepository;
     public CommentListResponse getCommentList(Long diaryId) {
-        List<Comment> commentList =commentRepositoty.findAllByDiaryId(diaryId);
+        List<Comment> commentList =commentRepository.findAllByDiaryId(diaryId);
         return new CommentListResponse(commentList);
     }
 }
