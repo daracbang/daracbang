255f06ffe0eef1bc4ade84e0511f3309b0646c07
우찬희 <8dener@gmail.com>
2023-11-10 16:51:10 +0900

    feature<server>: JWT exception, 회원 정보 API (소개글 용도) 추가
    Merge: Merge branch 'feature/BE/memberinfo' into 'develop/BE'
    
    * feat<server>: 회원 정보 조회 api 추가(소개글 용도)
    
    * feat<server>: jwt 인증 실패 exception 추가
    
    
    See merge request s09-final/S09P31A503!32

--- backend/apiserver/src/main/java/a503/daracbang/domain/member/util/JwtUtil.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/member/util/JwtUtil.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
+import a503.daracbang.domain.member.exception.MemberErrorCode;
+import a503.daracbang.global.exception.CustomException;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.MalformedJwtException;
@@ -52,16 +54,20 @@ public class JwtUtil {
 		try {
 			Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwt).getBody();
 			return true;
-		} catch (MalformedJwtException | SecurityException e) {
-			log.error("Invalid JWT signature");
-		} catch (ExpiredJwtException e) {
-			log.error("Expired JWT token");
-		} catch (UnsupportedJwtException e) {
-			log.error("Unsupported JWT token");
-		} catch (IllegalArgumentException e) {
-			log.error("Empty JWT token");
+		} catch (Exception e) {
+			throw new CustomException(MemberErrorCode.INVALID_JWT);
 		}
-		return false;
+
+		// catch (MalformedJwtException | SecurityException e) {
+		// 	log.error("Invalid JWT signature");
+		// } catch (ExpiredJwtException e) {
+		// 	log.error("Expired JWT token");
+		// } catch (UnsupportedJwtException e) {
+		// 	log.error("Unsupported JWT token");
+		// } catch (IllegalArgumentException e) {
+		// 	log.error("Empty JWT token");
+		// }
+		// return false;
 	}
 
 	public Long getIdFromJwt(String jwt) {
