39d4ed0ceb9c0ad2a28808eb7b62bf7b430c782a
JoInHyeok <ss112d86@gmail.com>
2023-11-15 12:36:01 +0900

    fix: 실행 에러 수정

--- frontend/daracbang/src/components/DayDiary.tsx
+++ frontend/daracbang/src/components/DayDiary.tsx
@@ -1,4 +1,4 @@
-import React,  from "react";
+import React from "react";
 import { Card, Typography } from "@mui/material";
 import { DiaryDetail } from "../api/diaryApi";
 import { formatDate } from "../utils/dateUtil";
