cb7471748f0ba46e2329e398eff91d03d2ae6d9d
권민재 <minjea917@gmail.com>
2023-11-16 12:02:42 +0900

    Refact/be/bgm
    Merge: Merge branch 'refact/BE/bgm' into 'develop/BE'
    
    * refact: create url에불필요한 memberId 제거
    
    * refact: url에서 video id 파싱
    
    * refact: bgm url과 name 만 받도록 수정
    
    
    See merge request s09-final/S09P31A503!48

--- backend/apiserver/src/main/java/a503/daracbang/domain/bgm/dto/request/RegisterBgmRequest.java
+++ backend/apiserver/src/main/java/a503/daracbang/domain/bgm/dto/request/RegisterBgmRequest.java
@@ -4,22 +4,27 @@ import a503.daracbang.domain.bgm.entity.Bgm;
 import a503.daracbang.domain.member.entity.Member;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+/**
+ * 유튜브 동영상 ID 값이 아닌 링크로 대체할 때 사용
+ */
 @Getter
-public class RegisterBgmIdRequest {
+@NoArgsConstructor
+public class RegisterBgmRequest {
 
     @Size(min = 1, max = 100, message = "1 ~ 100 자 내로 입력해주세요.")
-    private String videoId;
-
-    @Size(min = 1, max = 200, message = "1 ~ 200 자 내로 입력해주세요.")
     private String bgmName;
 
-    public RegisterBgmIdRequest(String videoId, String bgmName) {
-        this.videoId = videoId;
+    @Size(min = 1, max = 200, message = "1 ~ 200 자 내로 입력해주세요.")
+    private String url;
+
+    public RegisterBgmRequest(String bgmName, String url) {
         this.bgmName = bgmName;
+        this.url = url;
     }
 
     public Bgm toEntity(Member member) {
-        return new Bgm(member, bgmName, videoId, "default url");
+        return new Bgm(member, bgmName, url);
     }
 }
